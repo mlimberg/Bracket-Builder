40b3af9c25e9ca9ca87e997695a15a134ff06d5a
mlimberg <limbergmike@gmail.com>
2017-02-21 15:47:12 -0700

    Add matchup route

--- lib/components/Matchup/Matchup.jsx
+++ lib/components/Matchup/Matchup.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 
-export class Bracket extends Component {
+export class Matchup extends Component {
   constructor() {
     super();
     this.state={
@@ -12,24 +12,19 @@ export class Bracket extends Component {
   render() {
     return (
       <div>
-        Bracket!
+        Matchup!
 
         <Link to={`/dashboard/${this.state.tournyName}`}>
           <button>Dashboard</button>
         </Link>
 
-        <Link to={`/teams/${this.state.tournyName}`}>
-          <button>Teams</button>
+        <Link to={`/bracket/${this.state.tournyName}`}>
+          <button>Bracket</button>
         </Link>
-
-        <Link to={`/matchup/${this.state.tournyName}`}>
-          <button>Matchup</button>
-        </Link>
-
 
       </div>
     )
   }
 }
 
-export default Bracket;
+export default Matchup;
