5b6b908d9d2e96d651b61271914b2fcd62d5b9bf
mlimberg <limbergmike@gmail.com>
2017-02-21 14:41:23 -0700

    Create two new routes and toggle join exisiting form display

--- lib/components/Home/Home.jsx
+++ lib/components/Home/Home.jsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router';
 
-const Home = () => {
-  return(
-    <div>HOME!</div>
-  )
+export class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      join: false,
+    }
+  }
+
+  joinExistingForm() {
+    if(this.state.join)
+    return (
+      <div>
+        <input placeholder='Enter Code'/>
+        <button>Join</button>
+      </div>
+    )
+  }
+
+  render() {
+    return(
+      <div>
+        <Link to='/new'>
+          <button>New Tournament</button>
+        </Link>
+
+        <button onClick={() => this.setState({ join: !this.state.join })}>Join Existing</button>
+        
+        {this.joinExistingForm()}
+      </div>
+    )
+  }
 }
 
 export default Home
---
+++ lib/components/NewTournamentForm/NewTournamentForm.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+export class NewTournamentForm extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        New Tourny Form!
+      </div>
+    )
+  }
+}
+
+export default NewTournamentForm;
