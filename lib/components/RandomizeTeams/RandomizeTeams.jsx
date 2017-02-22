5d5f34a69bea2228fb8b0cd0dcfb5f63de15a65f
mlimberg <limbergmike@gmail.com>
2017-02-22 15:43:49 -0700

    Show randomized teams in two divisions

--- lib/components/RandomizeTeams/RandomizeTeams.jsx
+++ lib/components/RandomizeTeams/RandomizeTeams.jsx
@@ -2,6 +2,7 @@ import './randomize-styles';
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import TournamentContainer from '../../containers/Tournament/TournamentContainer';
+import TeamCard from '../TeamCard/TeamCard';
 
 export class RandomizeTeams extends Component {
   constructor() {
@@ -11,15 +12,39 @@ export class RandomizeTeams extends Component {
     }
   }
 
+  eastDivision() {
+    const east = this.props.teams.filter((t, i) => i % 2 === 0)
+    return east.map((team, i) => {
+      return (
+        <div key={'east' + i}>
+          <TeamCard team={team.name} />
+        </div>
+      )
+    })
+  }
+
+  westDivision() {
+    const west = this.props.teams.filter((t, i) => i % 2 !== 0)
+    return west.map((team, i) => {
+      return (
+        <div key={'west' + i}>
+          <TeamCard team={team.name} />
+        </div>
+      )
+    })
+  }
+
   render() {
-    console.log(this.props.teams);
+
     return (
       <div>
         Randomize Teams!
 
         <h3>East</h3>
+        {this.eastDivision()}
 
         <h3>West</h3>
+        {this.westDivision()}
 
         <button>Randomize</button>
 
