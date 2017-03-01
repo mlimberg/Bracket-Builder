01ce2b68e2464be93290005911cf64ffeaa873d8
mlimberg <limbergmike@gmail.com>
2017-03-01 11:44:16 -0700

    Add style to champ page

--- lib/components/Champion/index.jsx
+++ lib/components/Champion/index.jsx
@@ -14,7 +14,22 @@ const Champion = (props) => {
       <section className='sub-header-section'>
         <h1 className='winner-h1'>Winner Winner Chicken Dinner!</h1>
         <h3 className='winner-h3'>The grand champion is:</h3>
-        <h1 className='champ-name'>{champion.name}</h1>
+        <span className='champ-name'
+              style={{ backgroundColor: champion.color }}>
+          {champion.name}
+        </span>
+
+        {/* <img className='chicken-dinner'
+             src={require('../../images/chicken_dinner.png')} /> */}
+
+        <div className='social-container'>
+          <h3>Rub it in</h3>
+          <div className='social-icons'>
+            <img src={require('../../images/facebook_icon.png')}/>
+            <img src={require('../../images/twitter_icon.png')}/>
+            <img src={require('../../images/instagram_icon.png')}/>
+          </div>
+        </div>
       </section>
     </div>
   )
